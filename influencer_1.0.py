6483ed3f0416fcf158914ddd961c2280a57f7f2c
DataScience Gnip <snelson@data-science-6.gnip.com>
2015-02-18 23:23:31 +0000

    updates to script

--- influencer_1.0.py
+++ influencer_1.0.py
@@ -1,20 +1,16 @@
-fn = 'oscars2014_v3rules_tweets.json'
-#fn = 'dove_body_image.txt'
+fn = '../dove_body_image.txt'
 import json
 import sys
-import pandas as pd 
+#import pandas as pd 
 import codecs
 from datetime import datetime
-from collections import Counter
-data_all = pd.DataFrame()
-line_num=0
-
+#from collections import Counter
+#data_all = pd.DataFrame()
 
 label_counts = {
     'pos':{'pos':0,'neg':0},
     'neg':{'pos':0,'neg':0}
     }
-
 
 parent_follower = {} #{'parentID':['followerID_1','followerID_2']}
 
@@ -25,45 +21,60 @@ def add_to_list(mylist,value):
     return mylist
 
 #build labels data
+line_num=0
 with codecs.open(fn) as tweets:
     for line in tweets:
         line_num+=1
+#        if line_num > 100:
+#            break
         try:
-            if line !="\n":
-                data = json.loads(line)
-                #body = data["body"]
-                #preferredUsername = data["actor"]["preferredUsername"]
-                #displayName = data["actor"]["displayName"]
-                sourceID  = data["actor"]["id"].split(":")[2]
-                rule_tag =  data["gnip"]["matching_rules"][0]["tag"][:3]
-                postedTime = datetime.strptime(data["postedTime"],format="$Y-%m-%dT%H:%M:%S.000Z")
-                labels[sourceID]=labels.get(sourceID,{'pos':[],'neg':[]})
-                labels[sourceID][rule_tag].append(postedTime)
-        except ValueError:
-            sys.write.stderr("ValueError on line:{}".format(line_num))
+            data = json.loads(line)
+            #body = data["body"]
+            #preferredUsername = data["actor"]["preferredUsername"]
+            #displayName = data["actor"]["displayName"]
+            sourceID  = int(data["actor"]["id"].split(":")[2])
+            rule_tag =  data["gnip"]["matching_rules"][0]["tag"][:3]
+            postedTime = datetime.strptime(data["postedTime"], "%Y-%m-%dT%H:%M:%S.000Z")
+            labels[sourceID]=labels.get(sourceID,{'pos':[],'neg':[]})
+            labels[sourceID][rule_tag].append(postedTime)
+        except (ValueError,KeyError):
+            sys.stderr.write("Error on line:{}\n".format(line_num))
             continue
 
 #build the follower graph
-with open('follower_graph.txt','r') as f:
-    for row in f.readlines:
+with open('../follower_graph.txt','r') as f:
+    for row in f.readlines():
         l=row.split('\t')
-        parent=l[1]
-        follower=l[0]
+        parent=int(l[1])
+        follower=int(l[0])
         parent_follower[parent]=add_to_list(parent_follower.get(parent,[]),follower)
 
 
 #increments label counts
-label_tuple = ('pos','neg')
+keyerrors1=0
+keyerrors2=0
 for source in labels:
-    followers = parent_follwer[source]
+    try:
+        followers = parent_follower[source]
+    except KeyError:
+        keyerrors1+=1
+        sys.stderr.write("  source: KeyError on {}; total KeyErrors:{}\n".format(source,keyerrors1))
+        continue
     for source_label in labels[source]:
         for source_date in labels[source][source_label]:
             for follower in followers:
-                for follower_label in labels[follower]:
-                    for follower_date in labels[follower][follower_label]:
-                        if source_date<follower_date:
-                            label_counts[source_label][follower_label]+=1
+                try: 
+                    for follower_label in labels[follower]:
+                        for follower_date in labels[follower][follower_label]:
+                            sys.stderr.write("sourceDate:{},syncDate:{}".format(source_date,follower_date))
+                            if source_date<follower_date:
+                                sys.stderr.write("COUNT IT, label_counts[source_label][follower_label]:".format(label_counts[source_label][follower_label]))
+                                label_counts[source_label][follower_label]+=1
 
+                except KeyError:
+                    keyerrors2+=1
+                    sys.stderr.write("follower: KeyError on {}; total KeyErrors:{}\n".format(follower,keyerrors2))
 #print data
-print json.dumps(label_counts)
+with open('influencer_1.0.out','wb') as f:
+    f.write(json.dumps(label_counts))
 
